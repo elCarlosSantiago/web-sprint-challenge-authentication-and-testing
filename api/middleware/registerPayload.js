da8b1a3d16957b704a30be30901d87486fe67631
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-23 16:43:38 -0400

    registerpayload midd up

--- api/middleware/registerPayload.js
+++ api/middleware/registerPayload.js
@@ -2,12 +2,12 @@ const db = require('../../data/dbConfig');
 module.exports = async (req, res, next) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    return res.status(400).json({ message: 'username and password required' });
+    return res.status(401).json({ message: 'username and password required' });
   }
   const userCheck = await db('users').where('username', username).first();
   if (!userCheck) {
     next();
   } else {
-    return res.status(400).json({ message: 'username taken' });
+    return res.status(401).json({ message: 'username taken' });
   }
 };
--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,7 @@
 const server = require('./server');
 const request = require('supertest');
 const db = require('../data/dbConfig.js');
+const jwtDecode = require('jwt-decode');
 
 beforeAll(async () => {
   await db.migrate.rollback();
@@ -19,13 +20,75 @@ it('process.env.NODE_ENV must be "testing"', () => {
 });
 
 describe('Auth endpoints', () => {
-  describe('[POST] /register', () => {
-    it('registers a new user to the db', async () => {
-      await request(server)
+  describe('[POST] /api/auth/register', () => {
+    let newUser;
+    let res;
+    beforeEach(async () => {
+      res = await request(server)
         .post('/api/auth/register')
         .send({ username: 'shrek', password: '12345' });
-      const shrek = await db('users').where('username', 'shrek').first();
-      expect(shrek).toMatchObject({ username: 'shrek' });
+      newUser = await db('users').where('username', 'shrek').first();
+    });
+    it('registers a new user to the db', async () => {
+      expect(newUser).toMatchObject({ username: 'shrek' });
+    });
+    it('returns different password than provided (hashed)', async () => {
+      expect(newUser.password).not.toBe('12345');
+    });
+    it('returns a status 201', async () => {
+      expect(res.status).toBe(201);
+    });
+  });
+  describe('[POST] /api/auth/login', () => {
+    it('returns message on valid credentials', async () => {
+      const res = await request(server)
+        .post('/api/auth/login')
+        .send({ username: 'Guy', password: '1234' });
+      expect(res.body.message).toMatch(`welcome, Guy`);
+    },500);
+    it('returns token on valid login', async () => {
+      let res = await request(server)
+        .post('/api/auth/login')
+        .send({ username: 'Guy', password: '1234' });
+      let decoded = jwtDecode(res.body.token);
+      expect(decoded).toHaveProperty('iat');
+      expect(decoded).toHaveProperty('exp');
+      expect(decoded).toMatchObject({
+        subject: 1,
+        username: 'Guy',
+      });
+      res = await request(server)
+        .post('/api/auth/login')
+        .send({ username: 'Guyguy', password: '1234' });
+      decoded = jwtDecode(res.body.token);
+      expect(decoded).toHaveProperty('iat');
+      expect(decoded).toHaveProperty('exp');
+      expect(decoded).toMatchObject({
+        subject: 2,
+        username: 'Guyguy',
+      });
+    },500);
+  });
+});
+
+describe('Auth middleware', () => {
+  describe('registerPayload', () => {
+    it('responds with status 400 if username or password missing', async () => {
+      const resMissingPass = await request(server).post('/api/auth/register').send({
+        username: 'shrek',
+      });
+      const resMissingUser = await request(server).post('/api/auth/register').send({
+        password: 'shrek',
+      });
+      expect(resMissingPass.status).toBe(401);
+      expect(resMissingUser.status).toBe(401);
+    });
+    it('responds with username taken if taken', async () => {
+      const res = await request(server).post('/api/auth/register').send({
+        username: 'Guy',
+        password: '12345',
+      });
+      expect(res.body).toMatchObject({ message: 'username taken' });
     });
   });
 });
