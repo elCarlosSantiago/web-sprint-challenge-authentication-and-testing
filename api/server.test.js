aee2e4a8f9650f6cb1f8fdb8524c18ec7cdbc230
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-23 14:29:58 -0400

    test suite set up

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,19 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const server = require('./server');
+const request = require('supertest');
+const db = require('../data/dbConfig.js');
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+beforeEach(async () => {
+  await db('users').truncate();
+  await db.seed.run();
+});
+afterAll(async () => {
+  await db.destroy();
+});
+
+it('process.env.NODE_ENV must be "testing"', () => {
+  expect(process.env.NODE_ENV).toBe('testing');
+});
